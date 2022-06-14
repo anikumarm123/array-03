c522526522176d22eaec65ad9cf88c98c4d85884
muthukumar <anikumar2825@gmail.com>
2022-06-14 15:38:54 +0530

    addfile.

---
+++ script.js
@@ -0,0 +1,29 @@
+// shift() //
+// let num = [ 10,20,30,40,];
+// console.log(num.shift());
+// console.log(num);
+
+//unshift()//
+// let num = [ 10,20,30,40,];
+// console.log(num.unshift(9));
+// console.log(num);
+
+// sort() //
+// let Name = ["muthu","kumar","arul","magesh"];
+// console.log(Name.sort());
+
+// slice() //
+//  let animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
+//  console.log(animals.slice(3));
+ 
+
+// reverse //
+// let Name = ["muthu","ani","kumar","magesh"];
+// console.log(Name.reverse());
+
+// splice() //
+// let months = ['Jan','April', 'June','may','auc'];
+// b = months.splice(2,2,"March");
+// b = months.splice(3,2);
+// console.log(months);
+// console.log(b);
